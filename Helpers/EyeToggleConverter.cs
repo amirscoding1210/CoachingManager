f294a43af81d62719c178aae6db6d2d4a21d1a4b
amirulislam1210 <adamirulislam76@gmail.com>
2025-08-02 18:15:34 +0600

    password Eye toggle button added

---
+++ Helpers/EyeToggleConverter.cs
@@ -0,0 +1,22 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace CoachingCenterApp.Helpers
+{
+    public class EyeToggleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (value is bool boolValue && boolValue)
+                ? PackIconKind.Eye
+                : PackIconKind.EyeOff;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
